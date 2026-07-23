62d28fe8c4df76d11d73d5ebccbab7379533da2d
Amonullo <amonullo436@gmail.com>
2026-07-23 16:10:43 +0500

    Fixed the error

--- OrderService/OrderService.Application/Features/Validators/CreateOrderRequestValidator.cs
+++ OrderService/OrderService.Application/Features/Validators/CreateOrderRequestValidator.cs
@@ -7,15 +7,15 @@ public class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
 {
     public CreateOrderRequestValidator()
     {
-        RuleFor(x => x.CustomerId).GreaterThanOrEqualTo(0).WithMessage("Customer Id must be greater than 0");
+        RuleFor(x => x.CustomerId).GreaterThan(0).WithMessage("Customer Id must be greater than 0");
 
-        RuleFor(x => x.ShopperAssistantId).GreaterThanOrEqualTo(0)
+        RuleFor(x => x.ShopperAssistantId).GreaterThan(0)
             .WithMessage("Shopper Assistant Id must be greater than 0");
 
         RuleForEach(x => x.Items).ChildRules(item =>
         {
-            item.RuleFor(i => i.ProductId).GreaterThanOrEqualTo(0).WithMessage("Product Id must be greater than 0");
-            item.RuleFor(i => i.Quantity).GreaterThanOrEqualTo(0).WithMessage("Quantity must be greater than 0");
+            item.RuleFor(i => i.ProductId).GreaterThan(0).WithMessage("Product Id must be greater than 0");
+            item.RuleFor(i => i.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than 0");
         });
     }
 }
