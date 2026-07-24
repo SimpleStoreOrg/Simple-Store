ae3b554eee46cf6b6c70cc713795460f6772c484
Amonullo <amonullo436@gmail.com>
2026-07-24 11:45:07 +0500

    Modified and created new DTO's (U-S)

--- UserService/UserService.Application/DTOs/Request/CreateShopperAssistantRequest.cs
+++ UserService/UserService.Application/DTOs/Request/CreateShopperAssistantRequest.cs
@@ -1,9 +1,13 @@
+using UserService.Domain.Enums;
+
 namespace UserService.Application.DTOs.Request;
 
-public class UpdateUserRequest
+public class CreateShopperAssistantRequest
 {
     public string Name { get; set; }
     public string Surname { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
+    public ShopperAssistantPosition Position { get; set; }
     public string PhoneNumber { get; set; }
 }
--- UserService/UserService.Application/DTOs/Request/UpdateShopperAssistantRequest.cs
+++ UserService/UserService.Application/DTOs/Request/UpdateShopperAssistantRequest.cs
@@ -1,9 +1,12 @@
+using UserService.Domain.Enums;
+
 namespace UserService.Application.DTOs.Request;
 
-public class UpdateUserRequest
+public class UpdateShopperAssistantRequest
 {
     public string Name { get; set; }
     public string Surname { get; set; }
     public string Email { get; set; }
+    public ShopperAssistantPosition Position { get; set; }
     public string PhoneNumber { get; set; }
 }
